798e39208d42ee6c288db05ed5d66d936f1f15c3
jp-quintana <juanquintana1996@gmail.com>
2024-09-25 16:54:48 -0300

    add bcrypt

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { promises as fs } from 'fs';
 import { v4 as uuid } from 'uuid';
+import * as bcrypt from 'bcrypt';
 
 const filePath = './src/data/users.json';
 
@@ -15,8 +16,12 @@ export class AuthController {
 
       if (!user) return res.status(404).json({ message: 'User not found' });
 
-      if (user.password !== password)
-        return res.status(401).json({ message: 'Unauthorized' });
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch)
+        return res
+          .status(401)
+          .json({ message: 'Email or password is incorrect' });
 
       return res.status(201).json(user);
     } catch (error: any) {
@@ -27,7 +32,7 @@ export class AuthController {
 
   static async register(req: Request, res: Response, _next: NextFunction) {
     try {
-      const { email } = req.body;
+      const { email, password } = req.body;
       const data = await fs.readFile(filePath, 'utf-8');
       const users = JSON.parse(data);
 
@@ -37,9 +42,17 @@ export class AuthController {
         return res.status(409).json({ message: 'User already exists' });
       }
 
+      const salt = await bcrypt.genSalt(10);
+      const encryptedPassword = await bcrypt.hash(password, salt);
+
       const updatedUsers = [
         ...users,
-        { id: uuid(), ...req.body, accessToken: '123' },
+        {
+          id: uuid(),
+          ...req.body,
+          password: encryptedPassword,
+          accessToken: '123',
+        },
       ];
 
       await fs.writeFile(filePath, JSON.stringify(updatedUsers, null, 2));
