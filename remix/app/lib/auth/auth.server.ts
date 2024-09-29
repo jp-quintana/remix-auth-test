c99bbe865d541c336ca54aad46901616e1bfbe96
jp-quintana <juanquintana1996@gmail.com>
2024-09-29 12:59:23 -0300

    wip: oauth

--- remix/app/lib/auth/auth.server.ts
+++ remix/app/lib/auth/auth.server.ts
@@ -2,6 +2,7 @@ import { Authenticator } from 'remix-auth';
 import { sessionStorage } from '~/lib/auth/session.server';
 import { FormStrategy } from 'remix-auth-form';
 import { login } from '~/services/auth.service';
+import { OAuth2Strategy } from 'remix-auth-oauth2';
 
 export interface User {
   accessToken: string;
@@ -28,3 +29,33 @@ authenticator.use(
   }),
   'user-pass'
 );
+
+// authenticator.use(
+//   new OAuth2Strategy<
+//     User,
+//     { providers: "provider-name" },
+//     { id_token: string }
+//   >(
+//     {
+//       clientId: CLIENT_ID,
+//       clientSecret: CLIENT_SECRET,
+
+//       authorizationEndpoint: "https://provider.com/oauth2/authorize",
+//       tokenEndpoint: "https://provider.com/oauth2/token",
+//       redirectURI: "https://example.app/auth/callback",
+
+//       tokenRevocationEndpoint: "https://provider.com/oauth2/revoke", // optional
+
+//       codeChallengeMethod: "S256", // optional
+//       scopes: ["openid", "email", "profile"], // optional
+
+//       authenticateWith: "request_body", // optional
+//     },
+//     async ({ tokens, profile, context, request }) => {
+//       // here you can use the params above to get the user and return it
+//       // what you do inside this and how you find the user is up to you
+//       return await getUser(tokens, profile, context, request);
+//     },
+//   ),
+//   'oauth-google'
+// );
