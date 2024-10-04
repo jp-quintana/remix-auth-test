877197a6bbedb71ad6dfa44db6da5809bd67303d
jp-quintana <juanquintana1996@gmail.com>
2024-10-04 00:29:16 -0300

    wip4: oauth

--- remix/app/lib/auth/auth.server.ts
+++ remix/app/lib/auth/auth.server.ts
@@ -2,7 +2,7 @@ import { Authenticator } from 'remix-auth';
 import { sessionStorage } from '~/lib/auth/session.server';
 import { FormStrategy } from 'remix-auth-form';
 import { login } from '~/services/auth.service';
-import { OAuth2Strategy } from 'remix-auth-oauth2';
+// import { OAuth2Strategy } from 'remix-auth-oauth2';
 
 export interface User {
   accessToken: string;
@@ -30,22 +30,21 @@ authenticator.use(
   'user-pass'
 );
 
+// check OAuth2Strategy types
 // authenticator.use(
 //   new OAuth2Strategy<User, { provider: 'provider-name' }, { id_token: string }>(
 //     {
 //       clientId: process.env.GOOGLE_CLIENT_ID as string,
 //       clientSecret: process.env.GOOGLE_SECRET as string,
 
-//       authorizationEndpoint: 'https://provider.com/oauth2/authorize',
-//       tokenEndpoint: 'https://provider.com/oauth2/token',
+//       authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+//       tokenEndpoint: 'https://oauth2.googleapis.com/token',
 //       redirectURI: process.env.GOOGLE_OAUTH_REDIRECT_URL as string,
 
-//       // tokenRevocationEndpoint: "https://provider.com/oauth2/revoke", // optional
-
-//       // codeChallengeMethod: "S256", // optional
-//       scopes: ['openid', 'email', 'profile'],
-
-//       authenticateWith: 'request_body', // optional
+//       scopes: [
+//         'https://www.googleapis.com/auth/userinfo.profile',
+//         'https://www.googleapis.com/auth/userinfo.email',
+//       ], // optional
 //     },
 //     async ({ tokens, profile, context, request }) => {
 //       // here you can use the params above to get the user and return it
